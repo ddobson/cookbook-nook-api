b01682cc634749ec397f6f37f172c9761dccb753
ddobson <ddobson445@gmail.com>
2017-03-25 15:17:43 -0400

    Send Available Pages in Cookbook Serializer
    -  Cookbook serializer returns an array of available pages
    - Add methods to get available pages in model

--- app/models/cookbook.rb
+++ app/models/cookbook.rb
@@ -4,4 +4,32 @@ class Cookbook < ApplicationRecord
   belongs_to :user
   has_many :recipes
   validates :user, :title, :start_page, :end_page, presence: true
+
+  def recipe_page_ranges
+    recipes = self.recipes
+    recipes.map do |recipe|
+      start_page = recipe.start_page
+      end_page = recipe.end_page
+      (start_page..end_page)
+    end
+  end
+
+  def fetch_available_pages(pages, recipe_ranges)
+    recipe_ranges.each do |range|
+      indexes = build_page_indexes(pages, range)
+      next if indexes.length.zero?
+      slice_range = (indexes[0]..indexes[-1])
+      pages.slice!(slice_range)
+    end
+    pages
+  end
+
+  private
+
+  def build_page_indexes(pages, range)
+    indexes = range.map do |n|
+      pages.index(n)
+    end
+    indexes.compact.sort
+  end
 end
--- app/serializers/cookbook_serializer.rb
+++ app/serializers/cookbook_serializer.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 # :nodoc:
 class CookbookSerializer < ActiveModel::Serializer
-  attributes :id, :title, :start_page, :end_page
+  attributes :id, :title, :start_page, :end_page, :available_pages
+
+  def available_pages
+    cookbook_pages = (object.start_page..object.end_page).to_a
+    recipe_pages = object.recipe_page_ranges
+    object.fetch_available_pages(cookbook_pages, recipe_pages)
+  end
 end
