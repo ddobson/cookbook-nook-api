dcaadd2ec2a6109062508e42fe325128696478fe
ddobson <ddobson445@gmail.com>
2017-03-28 10:04:55 -0400

    Serialize cookbook recipes
    Return recipe ids and names for a given cookbook

--- app/serializers/cookbook_serializer.rb
+++ app/serializers/cookbook_serializer.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 # :nodoc:
 class CookbookSerializer < ActiveModel::Serializer
-  attributes :id, :title, :start_page, :end_page, :avail_pages
+  attributes :id, :title, :start_page, :end_page, :avail_pages, :recipes
 
   def avail_pages
     object.avail_pages.map(&:to_i)
+  end
+
+  def recipes
+    object.recipes.map do |recipe|
+      { id: recipe.id, name: recipe.name }
+    end
   end
 end
